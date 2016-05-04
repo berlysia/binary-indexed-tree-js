5d5c8bc158287b1bc93642a7f5266b171d1b1827
berlysia <berlysia@gmail.com>
2016-05-05 03:40:34 +0900

    initial commit

---
+++ src/bit.js
@@ -0,0 +1,101 @@
+"use strict";
+const assert = require('assert');
+
+/**
+ * @param {number} size
+ */
+function BinaryIndexedTree(size) {
+    const bit = Array(size + 1).fill(0);
+
+    function checkRange(x) {
+        return 0 <= x && x < size;
+    }
+
+    /**
+     * @param {number} idx - should be less than size of BIT
+     * @param {number} val
+     * O(log(N))
+     */
+    this.set = function set(idx, val) {
+        if (!checkRange(idx)) return false;
+        idx++;
+        for(let x = idx, l = bit.length - 1; x <= l; x += x & -x) {
+            bit[x] += val;
+        }
+        return true;
+    }
+
+    /**
+     * @param {number} idx - should be less than size of BIT
+     * @returns {number} sum of range [0..idx]
+     * O(log(N))
+     */
+    this.get = function get(idx) {
+        if (!checkRange(idx)) return undefined;
+        idx++;
+        let ans = 0;
+        for(let x = idx; x > 0; x -= x & -x) {
+            ans += bit[x];
+        }
+        return ans;
+    }
+
+    /**
+     * @returns {number} sum of range [0..size-1]
+     * O(log(N))
+     */
+    this.sum = function sum() {
+        return this.get(bit.length - 2);
+    }
+
+    function _comp(a, b) {
+        return a < b;
+    }
+
+    function _wrap(fn) {
+        return (a, b) => !fn(b, a);
+    }
+
+    /**
+     * @param {number} target
+     * @param {Function} comp
+     * @param {number} begin - begin index of lower-bound
+     * @param {number} end - end index of lower-bound
+     * @returns {number} index of lower-bound
+     * O(log(N) * log(N))
+     */
+    this.lowerBound = function(target, comp, begin, end) {
+        begin = begin || 0;
+        end = end || size;
+        if(begin < 0 && size <= begin) throw new Error('out-of-bounds');
+        if(end < 0 && size <= end) throw new Error('out-of-bounds');
+        if(typeof comp !== 'function') comp = _comp;
+
+        let mid;
+        while (end - begin > 1) {
+            mid = (begin + end) >> 1;
+            if (comp(this.get(mid), target)) {
+                begin = mid;
+            } else {
+                end = mid;
+            }
+        }
+        return comp(this.get(begin), target) ? end : begin;
+    }
+
+
+    /**
+     * @param {number} target
+     * @param {Function} comp
+     * @param {number} begin - begin index of upper-bound
+     * @param {number} end - end index of upper-bound
+     * @returns {number} index of upper-bound
+     * O(log(N) * log(N))
+     */
+    this.upperBound = function(target, comp, begin, end) {
+        if(typeof comp !== 'function') comp = _comp;
+        return this.lowerBound(target, _wrap(comp), begin, end);
+    }
+}
+
+module.exports = BinaryIndexedTree;
---
+++ test/bit.spec.js
@@ -0,0 +1,83 @@
+"use strict";
+const assert = require("power-assert");
+import BIT from '../src/bit';
+
+describe('test', function () {
+    let bit, size;
+    beforeEach(function () {
+        size = 10;
+        bit = new BIT(size);
+        for(let i = 0, l = size; i < l; ++i) {
+            bit.set(i, i);
+        }
+    });
+
+    it('works', function () {
+        const cusum = Array(size).fill(0)
+            .reduce((acc, x, i) => (acc.push(i ? i + acc[i - 1] : i), acc), []);
+        for(let i = 0, l = size; i < l; ++i) {
+            assert(bit.get(i) === cusum[i]);
+        }
+    });
+
+    it('access out of range returns undefined', function () {
+        assert(bit.get(size) === undefined);
+        assert(bit.get(-1) === undefined);
+    });
+
+    describe('#lowerBound, #upperBound', function () {
+        it('works', function () {
+            const bit = new BIT(size);
+            const target = 20;
+            [10,0,0,0,10,0,0,0,10,0].forEach((x, i) => bit.set(i, x));
+            const lb = bit.lowerBound(target);
+            const ub = bit.upperBound(target);
+            
+            // [10, 10, 10, 10, 20, 20, 20, 20, 30, 30]
+            //                  lb = 4          ub = 8
+
+            assert(target <= bit.get(lb));
+            assert(bit.get(lb - 1) < target);
+
+            assert(target < bit.get(ub));
+            assert(bit.get(ub - 1) <= target);
+
+            assert(ub - lb === 4);
+        });
+
+        it('too small target', function () {
+            const lb = bit.lowerBound(-1);
+            const ub = bit.upperBound(-1);
+            assert(lb === 0);
+            assert(ub === 0);
+        });
+
+        it('too large target', function () {
+            const lb = bit.lowerBound(bit.sum() + 1);
+            const ub = bit.upperBound(bit.sum() + 1);
+            assert(lb === size);
+            assert(ub === size);
+        });
+
+        it('custom comperator', function () {
+            const comp = (a, b) => a <= b;
+
+            const bit = new BIT(size);
+            const target = 20;
+            [10,0,0,0,10,0,0,0,10,0].forEach((x, i) => bit.set(i, x));
+            const lb = bit.lowerBound(target, comp);
+            const ub = bit.upperBound(target, comp);
+
+            // [10, 10, 10, 10, 20, 20, 20, 20, 30, 30]
+            //              lb = 3          ub = 7
+
+            assert(target < bit.get(lb));
+            assert(bit.get(lb - 1) <= target);
+
+            assert(target <= bit.get(ub));
+            assert(bit.get(ub - 1) < target);
+
+            assert(ub - lb === 4);
+        });
+    });
+});
