dceaa25c1aa5bda22d6e77dc4e87a09910782fe7
berlysia <berlysia@gmail.com>
2016-06-04 20:05:26 +0900

    chore(d.ts): Update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,17 @@
 interface BinaryIndexedTree {
     length: number;
     add(idx: number, val: number): boolean;
+    replace(idx: number, val: number): boolean;
     get(idx: number): number | void;
+    prefix(idx: number): number | void;
+    original(idx: number): number | void;
     sum(): number;
     lowerBound(target: number, comp?: (a: number, b: number) => boolean): number;
     upperBound(target: number, comp?: (a: number, b: number) => boolean): number;
+    find(check: (item: number, index?: number, bit?: BinaryIndexedTree) => boolean): number | void;
+    findIndex(check: (item: number, index?: number, bit?: BinaryIndexedTree) => boolean): number;
+    indexOf(target: number): number;
+    lastIndexOf(target: number): number;
     toArray(): Array<number>;
 }
 
